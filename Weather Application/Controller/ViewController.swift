9afc54f69c2bd20d36866e634e56e9eeb20d6126
mozeX <slem.cito@gmail.com>
2021-03-30 20:25:24 +0300

    Initial Commit

---
+++ Weather Application/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather Application
+//
+//  Created by mozeX on 29.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
